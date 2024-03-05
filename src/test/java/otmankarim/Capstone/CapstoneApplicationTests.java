ca024bf7b9a40bcf35d2079a370ed872f81995cf
Otman Karim <71219931+Ott00@users.noreply.github.com>
2024-03-05 15:28:01 +0100

    Initial commit

---
+++ src/test/java/otmankarim/Capstone/CapstoneApplicationTests.java
@@ -0,0 +1,13 @@
+package otmankarim.Capstone;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class CapstoneApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
