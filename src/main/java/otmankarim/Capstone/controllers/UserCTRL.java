1bada3353f3d4246709f3eaad23ecd5e04cc8aef
Otman Karim <71219931+Ott00@users.noreply.github.com>
2024-03-05 19:58:02 +0100

    Added /me endpoints

--- src/main/java/otmankarim/Capstone/controllers/UserCTRL.java
+++ src/main/java/otmankarim/Capstone/controllers/UserCTRL.java
@@ -4,8 +4,13 @@ import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.data.domain.Page;
 import org.springframework.http.HttpStatus;
 import org.springframework.security.access.prepost.PreAuthorize;
+import org.springframework.security.core.annotation.AuthenticationPrincipal;
+import org.springframework.validation.BindingResult;
+import org.springframework.validation.annotation.Validated;
 import org.springframework.web.bind.annotation.*;
 import otmankarim.Capstone.entities.User;
+import otmankarim.Capstone.exceptions.BadRequestException;
+import otmankarim.Capstone.payloads.UserDTO;
 import otmankarim.Capstone.services.UserSRV;
 
 import java.util.UUID;
@@ -33,6 +38,26 @@ public class UserCTRL {
     @ResponseStatus(HttpStatus.NO_CONTENT)
     public void delete(@PathVariable UUID id) {
         this.userSRV.delete(id);
+    }
+
+    @GetMapping("/me")
+    public User getMe(@AuthenticationPrincipal User user) {
+        return this.userSRV.getUserById(user.getId());
+    }
+
+    @PostMapping("/me")
+    @ResponseStatus(HttpStatus.CREATED)
+    public User updateMe(@AuthenticationPrincipal User user, @RequestBody @Validated UserDTO updatedUser, BindingResult validation) {
+        if (validation.hasErrors()) {
+            throw new BadRequestException(validation.getAllErrors());
+        }
+        return this.userSRV.updateUserById(updatedUser, user.getId());
+    }
+
+    @DeleteMapping("/me")
+    @ResponseStatus(HttpStatus.NO_CONTENT)
+    public void deleteMe(@AuthenticationPrincipal User user) {
+        this.userSRV.delete(user.getId());
     }
 
 }
