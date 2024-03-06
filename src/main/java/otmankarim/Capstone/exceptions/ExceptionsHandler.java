bb2b568010262f083935c2445c8047dab52e53bd
Otman Karim <71219931+Ott00@users.noreply.github.com>
2024-03-06 15:14:29 +0100

    fixed access denied handler

--- src/main/java/otmankarim/Capstone/exceptions/ExceptionsHandler.java
+++ src/main/java/otmankarim/Capstone/exceptions/ExceptionsHandler.java
@@ -2,11 +2,11 @@ package otmankarim.Capstone.exceptions;
 
 
 import org.springframework.http.HttpStatus;
+import org.springframework.security.access.AccessDeniedException;
 import org.springframework.web.bind.annotation.ExceptionHandler;
 import org.springframework.web.bind.annotation.ResponseStatus;
 import org.springframework.web.bind.annotation.RestControllerAdvice;
 
-import java.nio.file.AccessDeniedException;
 import java.time.LocalDateTime;
 import java.util.List;
 
